d1ce12c1a8a23998bb735c9c0f5de28e3938ec1b
qhsdnr0 <qhsdnr0@gmail.com>
2021-10-21 12:52:07 +0900

    Modify: users models, companies models
    Fix: delete documents.views

--- users/models.py
+++ users/models.py
@@ -4,10 +4,10 @@ from core.models import TimeStamp
 
 class User(TimeStamp):
     name              = models.CharField(max_length=100)
-    password          = models.CharField(max_length=200)
-    mobile_number     = models.CharField(max_length=100)
     kakao_id          = models.CharField(max_length=200)
     email             = models.EmailField(max_length=100, unique=True)
+    password          = models.CharField(max_length=200, null=True)
+    mobile_number     = models.CharField(max_length=100, null=True)
     college           = models.CharField(max_length=100, null=True)
     introduction      = models.CharField(max_length=1000, null=True)
     company_name      = models.CharField(max_length=100, null=True)
