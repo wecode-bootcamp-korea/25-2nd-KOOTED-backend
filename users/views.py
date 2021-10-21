2fc2b1eeae64a9b58403ddfae99e4f09c4f6e557
minho <maxkmh712@gmail.com>
2021-10-21 16:44:09 +0900

    [MODIFY] users/views.py KakaoSignInView 작성
    users app tests.py 작성

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import jwt, requests
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from users.models import User
+from my_settings  import SECRET_KEY, ALGORITHM
+
+
+class KakaoSignInView(View):
+  def get(self,request):
+    try:
+        kakao_token = request.headers.get('Authorization')
+        headers     = {'Content-Type': 'application/json; charset=utf-8', 'Authorization' : f'Bearer {kakao_token}'}
+        response    = requests.get('https://kapi.kakao.com/v2/user/me', headers = headers).json()
+        kakao_id    = response['id']
+        email       = response['kakao_account']['email']
+        name        = response['kakao_account']['profile']['nickname']
+
+        user, created = User.objects.get_or_create(
+            kakao_id  = kakao_id,
+            email     = email,
+            name      = name
+        )
+
+        access_token = jwt.encode({'user' : user.id}, SECRET_KEY, algorithm = ALGORITHM)
+
+        return JsonResponse({'access_token' : access_token}, status = 200 )
+
+    except KeyError:
+      return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+      
+    except ValueError:
+      return JsonResponse({'message' : 'INVALID_ERROR'}, status = 400)
