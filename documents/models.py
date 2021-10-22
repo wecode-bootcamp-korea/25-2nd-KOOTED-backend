1233ff768a79a6e4e141e32ef1b1ae477baccb97
qhsdnr0 <qhsdnr0@gmail.com>
2021-10-22 16:27:39 +0900

    Modify: documents.models
    Add: settings.logging
    
    Modify: .gitignore

--- documents/models.py
+++ documents/models.py
@@ -5,6 +5,8 @@ from users.models import User
 from posts.models import Post
 
 class Resume(TimeStamp):
+    title            = models.CharField(max_length=100)
+    status           = models.CharField(max_length=45)
     introduction     = models.CharField(max_length=1000, null=True)
     college          = models.CharField(max_length=100, null=True)
     resume_file_url  = models.CharField(max_length=2000, null=True)
@@ -14,7 +16,7 @@ class Resume(TimeStamp):
         db_table = 'resumes'
 
 class Application(TimeStamp):
-    recommender = models.CharField(max_length=100)
+    recommender = models.CharField(max_length=100, null=True)
     resume      = models.ForeignKey(Resume, on_delete=models.SET_NULL, null=True)
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
     post        = models.ForeignKey(Post, on_delete=models.CASCADE)
